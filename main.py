46a975e776144972d57ef2ccf1ace94e8e437ec6
Jeevan <your-email@gmail.comclear()>
2026-04-05 14:45:33 +0530

    Simple Calculator

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 print("Hello world")
 print("jeevan")
 print("Vikas K A")
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+while True:
+    a = input("Enter the Operator(+,-,*,/): ")
+
+    if a in ['+','-','*','/']:
+        if a == "+":
+            z = x + y
+        elif a == "-":
+            z = x - y
+        elif a == "*":
+            z = x * y
+        else:
+            z = x / y
+
+        print("Result:", z)
+        break
+    else:
+        print("Operator is not correct")
